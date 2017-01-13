b91ad84416e52b0b10d620dc0a3085ec1f278999
Dirk Tepe <tepeds@miamioh.edu>
2017-01-13 08:44:00 -0500

    Refactor shibboleth_idp::metadata::provider to address default source under Puppet 3
    Fixes #8 The use of param variables within other param default values does not work in Puppet 3. This change moves the default handling into the class body.

---
+++ spec/defines/metadata_provider_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe 'shibboleth_idp::metadata::provider', :type => :define do
+  let(:title) { 'TestSP' }
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge(:environment => 'test', :root_home => '/root', :service_provider => 'systemd')
+      end
+
+      describe 'file resource with defaults' do
+        let :pre_condition do
+          'class{"shibboleth_idp":
+              shib_install_base => "/junk/path"
+           }'
+        end
+
+        it { is_expected.to compile.with_all_deps }
+
+        context 'with defaults' do
+          
+          it { should contain_file('/junk/path/metadata/TestSP-metadata.xml').with({
+            :source => "/TestSP-metadata.xml",
+          })}
+
+        end
+
+        context 'with source_path => /file/source' do
+          let(:params) { {:source_path => '/file/source'} }
+
+          it { should contain_file('/junk/path/metadata/TestSP-metadata.xml').with({
+            :source => "/file/source/TestSP-metadata.xml",
+          })}
+
+        end
+      end
+    end
+  end
+end
