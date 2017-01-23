f727d5ae21296f6994a79f58e57f370a0febce03
Dirk Tepe <tepeds@miamioh.edu>
2017-01-23 16:35:52 -0500

    Remove useless validation and change param scope

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,30 +10,24 @@
 #
 #
 class shibboleth_idp::service {
-  # The base class must be included first because parameter defaults depend on it
-  if ! defined(Class['shibboleth_idp::params']) {
-    fail('You must include the shibboleth_idp::params class before using any shibboleth_idp defined resources')
-  }
-  validate_bool($shibboleth_idp::params::service_enable)
-  validate_bool($shibboleth_idp::params::service_manage)
 
-  case $shibboleth_idp::params::service_ensure {
+  case $shibboleth_idp::service_ensure {
     true, false, 'running', 'stopped': {
-      $_service_ensure = $shibboleth_idp::params::service_ensure
+      $_service_ensure = $shibboleth_idp::service_ensure
     }
     default: {
       $_service_ensure = undef
     }
   }
 
-  $_service_hasrestart = $shibboleth_idp::params::service_restart == undef
+  $_service_hasrestart = $shibboleth_idp::service_restart == undef
 
-  if $shibboleth_idp::params::service_manage {
+  if $shibboleth_idp::service_manage {
     service { 'jetty':
       ensure     => $_service_ensure,
-      name       => $shibboleth_idp::params::service_name,
-      enable     => $shibboleth_idp::params::service_enable,
-      restart    => $shibboleth_idp::params::service_restart,
+      name       => $shibboleth_idp::service_name,
+      enable     => $shibboleth_idp::service_enable,
+      restart    => $shibboleth_idp::service_restart,
       hasrestart => $_service_hasrestart,
     }
   }
