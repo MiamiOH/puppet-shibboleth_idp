ba60d25d7156bda441814e4ea05d4b9ea4e2b583
Dirk Tepe <tepeds@miamioh.edu>
2017-03-30 09:49:14 -0400

    Fix issue caused by refactoring of jetty log settings

--- manifests/jetty.pp
+++ manifests/jetty.pp
@@ -89,7 +89,8 @@ class shibboleth_idp::jetty (
   # Create the IdP Jetty base. This uses a directory of files
   # which are overridden by templates when necessary.
   $jetty_files = [ "${shibboleth_idp::idp_jetty_base}/lib", "${shibboleth_idp::idp_jetty_base}/lib/logging",
-  "${shibboleth_idp::idp_jetty_base}/lib/ext", "${shibboleth_idp::idp_jetty_base}/tmp", $idp_jetty_log_dir ]
+    "${shibboleth_idp::idp_jetty_base}/lib/ext", "${shibboleth_idp::idp_jetty_base}/tmp",
+  "${shibboleth_idp::idp_jetty_base}/resources", $idp_jetty_log_dir ]
 
   file { $shibboleth_idp::idp_jetty_base:
     ensure  => directory,
