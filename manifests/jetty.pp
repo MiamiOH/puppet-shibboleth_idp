b9bf88f902827916f816e13962cfec9ea49c9d7b
Andrew Farler <farlerac@miamioh.edu>
2023-10-19 12:21:13 -0400

    fix logic for start.ini path

--- manifests/jetty.pp
+++ manifests/jetty.pp
@@ -160,10 +160,9 @@ class shibboleth_idp::jetty (
     }
   }
 
-  $start_ini_path = $shib_major_version ? {
-    '3'     => $shibboleth_idp::idp_jetty_base,
-    '4'     => "${shibboleth_idp::idp_jetty_base}/start.d",
-    default => $shibboleth_idp::idp_jetty_base,
+  $start_ini_path = versioncmp($jetty_version, '9.4') ? {
+    -1      => $shibboleth_idp::idp_jetty_base,
+    default => "${shibboleth_idp::idp_jetty_base}/start.d",
   }
 
   file { "${start_ini_path}/start.ini":
